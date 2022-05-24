c6beda7df7742ee36dcbe6454fd1d4f3e8ac3b32
Damian Flynn <info@damianflynn.com>
2022-05-24 13:31:16 +0100

    upper and lower allowed

--- modules/loganalytics/variables.tf
+++ modules/loganalytics/variables.tf
@@ -2,7 +2,7 @@ variable "environment" {
   description = "Name of the Environment (e.g., D, Q, S, P)"
   default = "D"
   validation {
-    condition     = contains(lower(["D", "Q", "S", "P"]), var.environment)
+    condition     = contains(["D", "d", "Q", "q", "S", "s", "P", "p"], var.environment)
     error_message = "Environment must be 'D'ev, 'Q'A, 'S'tage or 'P'rod."
   } 
 }
